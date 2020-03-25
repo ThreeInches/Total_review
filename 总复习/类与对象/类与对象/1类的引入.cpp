47701bc583a1d9ddef5ed038fa0c57c1cd473ad8
Aixiaoluo <1330782530@qq.com>
2020-03-25 17:50:33 +0800

    类与对象

---
+++ 总复习/类与对象/类与对象/1类的引入.cpp
@@ -0,0 +1,32 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//
+//using namespace std;
+//
+//struct Student
+//{
+//	void SetStudentInfo(const char* name, const char* gender, int age)
+//	{
+//		strcpy(_name, name);
+//		strcpy(_gender, gender);
+//		_age = age;
+//	}
+//
+//	void PrintStudentInfo()
+//	{
+//		cout << _name << " " << _gender << " " << _age << " " << endl;
+//	}
+//
+//	char _name[20];
+//	char _gender[20];
+//	int _age;
+//};
+//
+//int main()
+//{
+//	Student stu;
+//	stu.SetStudentInfo("zhangsan", "mail", 18);
+//	stu.PrintStudentInfo();
+//	system("pause");
+//	return 0;
+//}
---
+++ 总复习/类与对象/类与对象/2类的定义.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class ClassName
+{
+	//类体：由成员函数和成员变量组成
+};//大括号的结尾必须加分号
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
