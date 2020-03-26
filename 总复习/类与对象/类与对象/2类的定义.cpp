8fdfd097ac4fc3a533b2996a373608c44cb55c2f
Aixiaoluo <1330782530@qq.com>
2020-03-26 19:32:02 +0800

    类与对象

--- 总复习/类与对象/类与对象/2类的定义.cpp
+++ 总复习/类与对象/类与对象/2类的定义.cpp
@@ -1,15 +1,29 @@
-#include <iostream>
-
-using namespace std;
-
-class ClassName
-{
-	//类体：由成员函数和成员变量组成
-};//大括号的结尾必须加分号
-
-int main()
-{
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//class ClassName
+//{
+//	//类体：由成员函数和成员变量组成
+//};//大括号的结尾必须加分号
+//
+////例如:
+//class Person
+//{
+//public:
+//	void ShowInfo()
+//	{
+//		cout << _name << " " << _sex << " " << _age << endl;
+//	}
+//public:
+//	char* _name;
+//	char* _sex;
+//	int _age;
+//};
+//
+//int main()
+//{
+//
+//	system("pause");
+//	return 0;
+//}
