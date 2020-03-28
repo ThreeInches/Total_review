7f7deadf3a98aadcb273c9245d70fd4f1cd260b3
Aixiaoluo <1330782530@qq.com>
2020-03-28 21:51:30 +0800

    运算符重载 赋值运算符重载 取地址运算符重载 const取地址运算符重载

---
+++ 总复习/Date/Date/Date.cpp
@@ -0,0 +1,23 @@
+#include "Date.h"
+using namespace std;
+
+Date(int year = 1900, int month = 1, int day = 1)
+{
+
+}
+
+Date(const Date& d);
+Date& operator=(const Date& d);
+Date operator+(int days);
+Date operator-(int days);
+int operator-(const Date& d);
+Date& operator++();
+Date operator++(int);
+Date& operator--();
+Date operator--(int);
+bool operator>(const Date& d)const;
+bool operator>=(const Date& d)const;
+bool operator<(const Date& d)const;
+bool operator<=(const Date& d)const;
+bool operator==(const Date& d)const;
+bool operator!=(const Date& d)const;
