9e0f625f9f15d8e8efabb1578e66f855748e2872
Aixiaoluo <1330782530@qq.com>
2020-04-25 21:57:57 +0800

    异常处理 异常的抛出和捕获 异常的重新抛出

---
+++ 总复习/异常处理/异常处理/1异常的概念.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+////C++异常处理的方式：当一个函数发现自己无法处理错误时，就可以抛出异常，让函数直接或间接的调用者来处理这个错误
+//
+////throw：当问题出现的时候，程序会抛出一个异常，这是通过throw关键字来完成的
+////catch：通过catch关键字来捕获异常，可以有多个catch进行捕获
+////try：try块中的代码标识将被激活的特定异常，它后面跟着一个或多个catch块
+//
+//int main()
+//{
+//
+//	try
+//	{
+//		//保护的标识代码
+//	}catch (ExceptionName e1)
+//	{
+//		//catch块
+//	}catch (ExceptionName e2)
+//	{
+//		//catch块
+//	}catch (ExceptionName eN)
+//	{
+//		//catch块
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
