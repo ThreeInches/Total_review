cde2bf13fe96775a0c2698fa66ac93e651aadf53
Aixiaoluo <1330782530@qq.com>
2020-05-01 22:45:55 +0800

    加注释

--- 总复习/C++类型转换/C++类型转换/2static_cast.cpp
+++ 总复习/C++类型转换/C++类型转换/2static_cast.cpp
@@ -1,14 +1,14 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-////static_cast用于非多态类型的转换，编译器执行的任何类型转换都可用static_cast，但它不能用于两个不相关类型进行转换
-//
-//int main()
-//{
-//	double d = 12.34;
-//	int a = static_cast<int>(d);
-//	cout << a << endl;
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+//static_cast用于非多态类型的转换，编译器执行的任何类型转换都可用static_cast，但它不能用于两个不相关类型进行转换
+
+int main()
+{
+	double d = 12.34;
+	int a = static_cast<int>(d);
+	cout << a << endl;
+	system("pause");
+	return 0;
+}
