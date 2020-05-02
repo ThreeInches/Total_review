7b9a28f29343f6d2fed81e2505d9a996ed194e0a
Aixiaoluo <1330782530@qq.com>
2020-05-02 22:00:55 +0800

    关联式容器上

---
+++ 总复习/关联式容器上/关联式容器上/1键值对.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+////键值对：用来表示具有一一对应关系的结构，该结构一般包含两个成员变量key和value，key代表键，value代表值，value表示与key对应的信息
+//
+//template<class T1, class T2>
+//class pair
+//{
+//public:
+//	typedef T1 fist_type;
+//	typedef T2 second_type;
+//	pair()
+//		:first(T1())
+//		, second(T2())
+//	{
+//
+//	}
+//
+//	pair(const T1& a, const T2& b)
+//		:first(a)
+//		, second(b)
+//	{
+//
+//	}
+//public:
+//	T1 first;
+//	T2 second;
+//};
+//
+//int main()
+//{
+//
+//	system("pause");
+//	return 0;
+//}
