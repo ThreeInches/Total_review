49b66eae0f671b9652b9785571059fff2a7af3cd
Aixiaoluo <1330782530@qq.com>
2020-05-12 21:16:12 +0800

    Inorder

--- 总复习/BinarySortTree/BinarySortTree/BinarySortTree.h
+++ 总复习/BinarySortTree/BinarySortTree/BinarySortTree.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stack>
+#include <vector>
 
 namespace gwp
 {
@@ -7,15 +9,18 @@ namespace gwp
 	{
 	public:
 		BSTNode(const T& data = T())
-			:m_pleft(nullptr)
-			, m_pright(nullptr)
+			:m_left(nullptr)
+			, m_right(nullptr)
 			, m_data(data)
 		{
 
 		}
-	public:
-		BSTNode<T>* m_pleft;
-		BSTNode<T>* m_pright;
+
+		template<class T>
+		friend class BSTree;
+	private:
+		BSTNode<T>* m_left;
+		BSTNode<T>* m_right;
 		T m_data;
 	};
 
@@ -33,7 +38,17 @@ namespace gwp
 
 		~BSTree()
 		{
+			Destory(m_pRoot);
+		}
 
+		void Destory(pNode root)
+		{
+			if (root)
+			{
+				Destory(root->m_left);
+				Destory(root->m_right);
+				delete root;
+			}
 		}
 
 		pNode Find(const T& data)
@@ -59,10 +74,10 @@ namespace gwp
 					pre = cur;
 					cur = cur->m_left;
 				}
-				else if (data > cur->data)
+				else if (data > cur->m_data)
 				{
 					pre = cur;
-					cur = cur->right;
+					cur = cur->m_right;
 				}
 				else
 				{
@@ -70,7 +85,7 @@ namespace gwp
 				}
 			}
 
-			cur = new Node<T>(data);
+			cur = new Node(data);
 			if (data < pre->m_data)
 			{
 				pre->m_left = cur;
@@ -85,7 +100,7 @@ namespace gwp
 
 		bool Erase(const T& data)
 		{
-			if (m_root == nullptr)
+			if (m_pRoot == nullptr)
 			{
 				return false;
 			}
@@ -100,14 +115,14 @@ namespace gwp
 					pre = cur;
 					cur = cur->m_left;
 				}
-				else if (data > cur->data)
+				else if (data > cur->m_data)
 				{
 					pre = cur;
-					cur = cur->right;
+					cur = cur->m_right;
 				}
 				else
 				{
-					return break;
+					break;
 				}
 			}
 
@@ -167,9 +182,29 @@ namespace gwp
 			}
 		}
 
-		void InOrder()
+		std::vector<T> InOrder()
 		{
+			std::stack<pNode> s;
+			std::vector<T> res;
+			pNode cur = m_pRoot;
 
+			while (cur || !s.empty())
+			{
+				for (; cur; cur = cur->m_left)
+				{
+					s.push(cur);
+				}
+
+				if (!s.empty())
+				{
+					cur = s.top();
+					res.push_back(cur->m_data);
+					s.pop();
+
+					cur = cur->m_right;
+				}
+			}
+			return res;
 		}
 	private:
 		pNode m_pRoot;
