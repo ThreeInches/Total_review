948bcd3fd3da57b9ba2c7a8b940be10bfd25d185
Aixiaoluo <1330782530@qq.com>
2020-05-17 22:20:03 +0800

    insert

--- 总复习/AVL/AVL/AVL.cpp
+++ 总复习/AVL/AVL/AVL.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include "AVL.h"
+//#include "AVL.h"
 
 using namespace std;
 
+template<class K, class V>
+void test(const K& k, const V& v)
+{
+	pair<K, V> p3(k, v);
+	pair<K, V> p4;
+}
+
 int main()
 {
-
+	pair<string, string> p("123", "abc");
+	pair<int, int> p2;
+	test(1, 2);
 	system("pause");
 	return 0;
 }
--- 总复习/AVL/AVL/AVL.h
+++ 总复习/AVL/AVL/AVL.h
@@ -1,90 +1,117 @@
-template<class K, class V>
-class AVLTreeNode
-{
-public:
-	AVLTreeNode(const pair<K, V>& kv)
-		:m_left(nullptr)
-		, m_right(nullptr)
-		, m_parent(nullptr)
-		, m_bf(0)
-		, m_kv(kv)
-	{
-
-	}
-public:
-	AVLTreeNode<K, V>* m_left;
-	AVLTreeNode<K, V>* m_right;
-	AVLTreeNode<K, V>* m_parent;
-
-	pair<K, V> m_kv;
-	
-	int m_bf;
-};
-
-template<class K, class V>
-class AVLTree
-{
-	typedef AVLTreeNode<K, V> Node;
-public:
-	AVLTree()
-		:m_root(nullptr)
-	{
-
-	}
-
-	V& operator [] (const K& key)
-	{
-		pair<Node*, bool> ret = insert(make_pair(key, V()));
-		return ret.first->m_kv.second;
-	}
-
-	pair<Node*, bool>& insert(const pair<K, V>& kv)
-	{
-		if (m_root = nullptr)
-		{
-			m_root = new Node(kv);
-			return make_pair(m_root, true);
-		}
-
-		Node* cur = m_root;
-		Node* parent = nullptr;
-		while (cur)
-		{
-			if (cur->m_kv.first < m_kv.first)
-			{
-				parent = cur;
-				cur = cur->m_right;
-			}
-			else if (cur->m_kv.first > m_kv.first)
-			{
-				parent = cur;
-				cur = cur->m_left;
-			}
-			else
-			{
-				return make_pair(cur, false>);
-			}
-		}
-
-		cur = new Node(kv);
-		if (parent->m_kv.first < m_kv.first)
-		{
-			parent->m_right = cur;
-			cur->m_parent = parent;
-		}
-		else
-		{
-			parent->m_left = cur;
-			cur->m_parent = parent;
-		}
-		return make_pair(cur, true);
-	}
-private:
-	Node* m_root = nullptr;
-};
-
-void TestAVLTree()
-{
-	AVLTree<int, int> t;
-	//t.insert(make_pair(1, 1));
-}
+//template<class K, class V>
+//class AVLTreeNode
+//{
+//public:
+//	AVLTreeNode(const pair<K, V>& kv)
+//		: m_left(nullptr)
+//		, m_right(nullptr)
+//		, m_parent(nullptr)
+//		, m_bf(0)
+//		, m_kv(kv)
+//	{
+//
+//	}
+//public:
+//	AVLTreeNode<K, V>* m_left;
+//	AVLTreeNode<K, V>* m_right;
+//	AVLTreeNode<K, V>* m_parent;
+//
+//	pair<K, V> m_kv;
+//	
+//	int m_bf;
+//};
+//
+//template<class K, class V>
+//class AVLTree
+//{
+//	typedef AVLTreeNode<K, V> Node;
+//public:
+//	AVLTree()
+//		:m_root(nullptr)
+//	{
+//
+//	}
+//
+//	V& operator [] (const K& key)
+//	{
+//		pair<Node*, bool> ret = insert(make_pair(key, V()));
+//		return ret.first->m_kv.second;
+//	}
+//
+//	pair<Node*, bool>& insert(const pair<K, V>& kv)
+//	{
+//		if (m_root = nullptr)
+//		{
+//			m_root = new Node(kv);
+//			return make_pair(m_root, true);
+//		}
+//
+//		Node* cur = m_root;
+//		Node* parent = nullptr;
+//		while (cur)
+//		{
+//			if (cur->m_kv.first < m_kv.first)
+//			{
+//				parent = cur;
+//				cur = cur->m_right;
+//			}
+//			else if (cur->m_kv.first > m_kv.first)
+//			{
+//				parent = cur;
+//				cur = cur->m_left;
+//			}
+//			else
+//			{
+//				return make_pair(cur, false>);
+//			}
+//		}
+//
+//		cur = new Node(kv);
+//		if (parent->m_kv.first < m_kv.first)
+//		{
+//			parent->m_right = cur;
+//			cur->m_parent = parent;
+//		}
+//		else
+//		{
+//			parent->m_left = cur;
+//			cur->m_parent = parent;
+//		}
+//
+//		while (parent)
+//		{
+//			if (cur = parent->m_left)
+//			{
+//				parent->m_bf -= 1;
+//			}
+//			else
+//			{
+//				parent->m_bf += 1;
+//			}
+//
+//			if ((parent->m_bf == 1) || (parent->m_bf == -1)
+//			{
+//				cur = cur->m_parent;
+//				parent = parent->m_parent;
+//			}
+//			else if (parent->m_bf == 0)
+//			{
+//				break;
+//			}
+//			else if ((parent->m_bf == 2) || (parent->m_bf == -2)
+//			{
+//				//Ðý×ª
+//			}
+//		}
+//
+//		return make_pair(cur, true);
+//	}
+//private:
+//	Node* m_root = nullptr;
+//};
+//
+//void TestAVLTree()
+//{
+//	AVLTree<int, int> t;
+//	//t.insert(make_pair(1, 1));
+//}
