48612b5cf362c86a1e3e2a79a5cda58f011551f8
Aixiaoluo <1330782530@qq.com>
2020-05-26 22:25:14 +0800

    set类的模拟实现/set.h

--- 总复习/set类的模拟实现/set类的模拟实现/set.cpp
+++ 总复习/set类的模拟实现/set类的模拟实现/set.cpp
@@ -4,6 +4,12 @@ int main()
 {
 	gwp::set<int> s;
 	s.insert(1);
+	s.insert(2);
+	s.insert(3);
+	s.insert(4);
+	s.insert(5);
+	s.insert(6);
+	s.insert(7);
 	system("pause");
 	return 0;
 }
--- 总复习/set类的模拟实现/set类的模拟实现/set.h
+++ 总复习/set类的模拟实现/set类的模拟实现/set.h
@@ -5,13 +5,8 @@ namespace gwp
 {
 	template<class K>
 	class set
-	{
+	{	
 	public:
-		void insert(const K& data)
-		{
-			m_t.Insert(data);
-		}
-	private:
 		class SetKeyOfValue
 		{
 		public:
@@ -21,6 +16,38 @@ namespace gwp
 			}
 		};
 
+		typedef typename RBTree<K, K, SetKeyOfValue>::Iterator Iterator;
+	public:
+		pair<Iterator, bool> insert(const K& data)
+		{
+			return m_t.Insert(data);
+		}
+
+		Iterator begin()
+		{
+			return m_t.begin();
+		}
+
+		Iterator end()
+		{
+			return m_t.end();
+		}
+
+		Iterator rbegin()
+		{
+			return m_t.rbegin();
+		}
+
+		Iterator rend()
+		{
+			return m_t.rend();
+		}
+
+		void inorder()
+		{
+			m_t.InOrder();
+		}
+	private:
 		RBTree<K, K, SetKeyOfValue> m_t;
 	};
 }
