170378ae6b72ccf4ca39b3c2ee22d7c231d587c2
Aixiaoluo <1330782530@qq.com>
2020-06-10 22:30:38 +0800

    rsa.h rsa.cpp test.cpp

--- 总复习/文件加密工具/文件加密工具/rsa.cpp
+++ 总复习/文件加密工具/文件加密工具/rsa.cpp
@@ -1,33 +1,16 @@
 #include <iostream>
+#include <algorithm>
 #include <time.h>
 #include "rsa.h"
 
-//加密
-DataType ecrept(DataType data, DataType ekey, DataType pkey);
-//解密
-DataType decrept(DataType data, DataType ekey, DataType pkey);
-bool isPrime(DataType data)
-{
-	if (data <= 0)
-	{
-		return false;
-	}
-	for (int i = 2; i <= sqrt(data); i++)
-	{
-		if (data%i == 0)
-		{
-			return false;
-			break;
-		}
-	}
-	return true;
-}
+using namespace std;
 
-DataType getPrime()
+//1、随机产生两个素数p，q
+DataType rsa::getPrime()
 {
-	srand(time(NULL));
+	srand(time(nullptr));
 	DataType prime;
-	while (true)
+	while (1)
 	{
 		prime = rand() % 100 + 2;
 		if (isPrime(prime))
@@ -35,51 +18,127 @@ DataType getPrime()
 			break;
 		}
 	}
+
 	return prime;
 }
 
-DataType getPkey(DataType prime1, DataType prime2)
+//判断一个数是否是素数
+bool rsa::isPrime(DataType data)
 {
-	return prime1*prime2;
+	if (data < 0)
+	{
+		return false;
+	}
+
+	for (int i = 2; i <= sqrt(data); i++)
+	{
+		if (data % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
-DataType getOrla(DataType prime1, DataType prime2)
+//2、计算n 其中n = p * q
+DataType rsa::getNkey(DataType prime1, DataType prime2)
 {
-	return (prime1 - 1)*(prime2 - 1);
+	return prime1 * prime2;
 }
 
-DataType getEkey(DataType orla)
+//3、求n的欧拉函数，计算f(n) = f(p) * f(q) = (p - 1)*(q - 1)
+DataType rsa::getOrla(DataType prime1, DataType prime2)
 {
-	srand(time(NULL));
-	DataType ekey;
+	return (prime1 - 1) * (prime2 - 1);
+}
+
+//4、求EKey随机取e  1 < e < f(n) 且e 与 f(n)互质
+DataType rsa::getEKey(DataType orla)
+{
+	srand(time(nullptr));
+	DataType EKey;
 	while (1)
 	{
-		ekey = rand() % orla;
-		if (ekey > 1 && getGcd(ekey, orla) == 1)
+		EKey = rand() % orla;
+		if (EKey > 1 && getCommonDivisor(EKey, orla) == 1)
 		{
-			return ekey;
+			return EKey;
 		}
 	}
 }
 
-DataType getDkey(DataType ekey, DataType orla)
+//5、求DKey计算e对于f(n)的模反元素d (d * e) % f(n) = 1
+DataType rsa::getDKey(DataType EKey, DataType orla)
 {
-	DataType dkey = orla / ekey;
+	DataType DKey;
+	DKey = orla / EKey;
 	while (1)
 	{
-		if ((dkey*ekey) % orla == 1)
+		if ((DKey * EKey) % orla == 1)
 		{
-			return dkey;
+			return DKey;
 		}
-		dkey++;
+		DKey++;
 	}
 }
 
-DataType getGcd(DataType data1, DataType data2)
+//加密过程：模幂运算 即a^b%c
+DataType rsa::encryption(DataType data, DataType EKey, DataType NKey)
 {
-	DataType residual;
-	while (residual = data2%data1)
+	//i: 0,……, 最后一位
+	DataType Ai = data;
+	DataType msgE = 1;
+	//data^Key%NKey
+	while (EKey)
 	{
-
+		if (EKey & 1)
+		{
+			msgE = (msgE * Ai) % NKey;
+		}
+		EKey >> 1;
+		Ai = (Ai * Ai) % NKey;
 	}
+
+	return msgE;
+}
+
+//解密过程：模幂运算 即a^b%c
+DataType rsa::decryption(DataType data, DataType DKey, DataType NKey)
+{
+	return (DataType)pow(data, DKey) % NKey;
+}
+
+//求两个数的最大公约数（辗转相除法）
+DataType rsa::getCommonDivisor(DataType data1, DataType data2)
+{
+	DataType res = data1 % data2;
+	while (res)
+	{
+		data1 = data2;
+		data2 = res;
+		res = data1 % data2;
+	}
+
+	return data2;
+}
+
+void rsa::getKeys()
+{
+	DataType prime1 = getPrime();
+	DataType prime2 = getPrime();
+	while (prime1 == prime2)
+	{
+		prime2 = getPrime();
+	}
+
+	DataType orla = getOrla(prime1, prime2);
+	m_Key.m_NKey = getNkey(prime1, prime2);
+	m_Key.m_EKey = getEKey(orla);
+	m_Key.m_Dkey = getDKey(m_Key.m_EKey, orla);
+}
+
+Key rsa::getKey()
+{
+	return m_Key;
 }
--- 总复习/文件加密工具/文件加密工具/rsa.h
+++ 总复习/文件加密工具/文件加密工具/rsa.h
@@ -2,3 +2,44 @@
 
 typedef long DataType;
 
+struct Key
+{
+	DataType m_EKey;
+	DataType m_Dkey;
+	DataType m_NKey;
+};
+
+//1、随机产生两个素数p，q
+//2、计算n 其中n = p * q
+//3、求n的欧拉函数，计算f(n) = f(p) * f(q) = (p - 1)*(q - 1)
+//4、随机取e  1 < e < f(n) 且e 与 f(n)互质
+//5、计算e对于f(n)的模反元素d (d * e) % f(n) = 1
+//6、产生公钥(e, n) 私钥(d, n)
+
+class rsa
+{
+public:
+	//1、随机产生两个素数p，q
+	DataType getPrime();
+	//判断一个数是否是素数
+	bool isPrime(DataType data);
+	//2、计算n 其中n = p * q
+	DataType getNkey(DataType prime1, DataType prime2);
+	//3、求n的欧拉函数，计算f(n) = f(p) * f(q) = (p - 1)*(q - 1)
+	DataType getOrla(DataType prime1, DataType prime2);
+	//4、求EKey随机取e  1 < e < f(n) 且e 与 f(n)互质
+	DataType getEKey(DataType orla);
+	//5、求DKey计算e对于f(n)的模反元素d (d * e) % f(n) = 1
+	DataType getDKey(DataType EKey, DataType orla);
+	//加密过程：模幂运算 即a^b%c
+	DataType encryption(DataType data, DataType EKey, DataType NKey);
+	//解密过程：模幂运算 即a^b%c
+	DataType decryption(DataType data, DataType DKey, DataType NKey);
+	//求两个数的最大公约数
+	DataType getCommonDivisor(DataType data1, DataType data2);
+	//产生所有的key
+	void getKeys();
+	Key getKey();
+private:
+	Key m_Key;
+};
