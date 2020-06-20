0df32e95edd493a00545e07c66e8db855bdf0122
Aixiaoluo <1330782530@qq.com>
2020-06-20 23:09:10 +0800

    把字符串转换成整数.cpp

---
+++ 总练习/把字符串转换成整数/把字符串转换成整数/把字符串转换成整数.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+	int StrToInt(string str)
+	{
+		if (str.empty())
+		{
+			return 0;
+		}
+
+		int flag = 1;
+		if (str[0] == '-')
+		{
+			flag = -1;
+			str[0] = '0';
+		}
+		else if (str[0] == '+')
+		{
+			flag = 1;
+			str[0] = '0';
+		}
+
+		int sum = 0;
+		for (int i = 0; i < str.size(); i++)
+		{
+			if (str[i] < '0' || str[i]>'9')
+			{
+				sum = 0;
+				break;
+			}
+
+			sum = sum * 10 + str[i] - '0';
+		}
+
+		return flag * sum;
+	}
+};
+
+int main()
+{
+	Solution a;
+	cout << a.StrToInt("123") << endl;
+	cout << a.StrToInt("-123") << endl;
+	system("pause");
+	return 0;
+}
