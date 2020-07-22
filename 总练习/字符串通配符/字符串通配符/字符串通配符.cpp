17f737fee4ed2096b8c0c1690d3bd30d731aa0d3
Aixiaoluo <1330782530@qq.com>
2020-07-22 22:39:22 +0800

    字符串通配符.cpp 统计每个月兔子的总数.cpp

--- 总练习/字符串通配符/字符串通配符/字符串通配符.cpp
+++ 总练习/字符串通配符/字符串通配符/字符串通配符.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
+bool match(const char* pattern, const char* str)
+{
+	if (*pattern == '\0' && *str == '\0')
+	{
+		return true;
+	}
+
+	if (*pattern == '\0' || *str == '\0')
+	{
+		return false;
+	}
+
+	if (*pattern == '?' || *pattern == *str)
+	{
+		return match(pattern + 1, str + 1);
+	}
+	else if(*pattern == '*')
+	{
+		return match(pattern + 1, str) || match(pattern, str + 1) || match(pattern + 1, str + 1);
+	}
+
+	return false;
+}
+
 int main()
 {
-
+	string pattern, str;
+	while (cin >> pattern >> str)
+	{
+		if (match(pattern.c_str(), str.c_str()))
+		{
+			cout << "true" << endl;
+		}
+		else
+		{
+			cout << "false" << endl;
+		}
+	}
 	system("pause");
 	return 0;
 }
--- 总练习/统计每个月兔子的总数/统计每个月兔子的总数/统计每个月兔子的总数.cpp
+++ 总练习/统计每个月兔子的总数/统计每个月兔子的总数/统计每个月兔子的总数.cpp
@@ -1,10 +1,90 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+//int Fibonacci(int n)
+//{
+//	unordered_map<int, int> filter;
+//	if (n == 0)
+//	{
+//		return 0;
+//	}
+//	if (n == 1 || n == 2)
+//	{
+//		return 1;
+//	}
+//
+//	int ppre = 0;
+//	if (filter.find(n - 2) == filter.end())
+//	{
+//		ppre = Fibonacci(n - 2);
+//		filter.insert({ n - 2, ppre });
+//	}
+//	else
+//	{
+//		ppre = filter[n - 2];
+//	}
+//
+//	int pre = 0;
+//	if (filter.find(n - 1) == filter.end())
+//	{
+//		pre = Fibonacci(n - 1);
+//		filter.insert({ n - 1, pre });
+//	}
+//	else
+//	{
+//		pre = filter[n - 1];
+//	}
+//
+//	return pre + ppre;
+//}
+
+//int Fibonacci(int n)
+//{
+//	if (n == 0)
+//	{
+//		return 0;
+//	}
+//	if (n == 1 || n == 2)
+//	{
+//		return 1;
+//	}
+//	int l1 = 1;
+//	int l2 = 1;
+//	int l = 1;
+//	while (n > 2)
+//	{
+//		l = l1 + l2;
+//		l1 = l2;
+//		l2 = l;
+//		n--;
+//	}
+//	return l;
+//}
+
+int Fibonacci(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	return (Fibonacci(n - 1) + Fibonacci(n - 2));
+}
+
 int main()
 {
-
+	int n;
+	while (cin >> n)
+	{
+		cout << Fibonacci(n) << endl;
+	}
 	system("pause");
 	return 0;
 }
